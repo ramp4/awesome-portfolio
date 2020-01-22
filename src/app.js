e9df0e4a2f0174f9efad8b9eba945ea425b449ad
ramp4 <rampatv13@gmail.com>
2020-01-22 23:34:47 +0300

    feat: add news app

--- src/app.js
+++ src/app.js
@@ -11,11 +11,14 @@ function App() {
       <div className="portfolio">
         <Task task={{ name: 'Curriculum Vitae', url: 'https://github.com/ramp4/rsschool-cv/blob/gh-pages/cv.md' }} />
         <Task task={{ name: 'Fancy Weather', url: 'https://vveather.netlify.com/' }} />
+        <Task task={{ name: 'News', url: 'https://news-page.netlify.com/' }} />
+        <Task task={{ name: 'Image-api', url: 'https://image-appi.netlify.com/' }} />
         <Task task={{ name: 'Repair Design Project', url: 'https://repair-project.netlify.com/' }} />
         <Task task={{ name: 'The Yalow', url: 'https://the-yalow.netlify.com//' }} />
         <Task task={{ name: 'Hexal', url: 'https://hexalogon.netlify.com' }} />
         <Task task={{ name: 'NeutronMail', url: 'https://neutron-email.netlify.com/' }} />
-        <Task task={{ name: 'Image-api', url: 'https://image-appi.netlify.com/' }} />
+
+
       </div>
     </div>
   );
