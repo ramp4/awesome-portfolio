c84f88e6350aa790c0353703e36f40d5f0b9cd9c
ramp4 <rampatv13@gmail.com>
2020-01-24 21:19:20 +0300

    add: vk task, add news task

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ function App() {
     </canvas>
       <div className="portfolio">
         <Task task={{ name: 'Curriculum Vitae', url: 'https://github.com/ramp4/rsschool-cv/blob/gh-pages/cv.md' }} />
-        <Task task={{ name: 'Fancy Weather', url: 'https://vveather.netlify.com/' }} />
-        <Task task={{ name: 'News', url: 'https://news-page.netlify.com/' }} />
-        <Task task={{ name: 'Image-api', url: 'https://image-appi.netlify.com/' }} />
-        <Task task={{ name: 'Repair Design Project', url: 'https://repair-project.netlify.com/' }} />
-        <Task task={{ name: 'The Yalow', url: 'https://the-yalow.netlify.com//' }} />
-        <Task task={{ name: 'Hexal', url: 'https://hexalogon.netlify.com' }} />
-        <Task task={{ name: 'NeutronMail', url: 'https://neutron-email.netlify.com/' }} />
-
+        <Task task={{ name: 'Fancy Weather - JS', url: 'https://vveather.netlify.com/' }} />
+        <Task task={{ name: 'VK - React+Redux', url: 'https://vk-api.netlify.com/' }} />
+        <Task task={{ name: 'News - React', url: 'https://news-page.netlify.com/' }} />
+        <Task task={{ name: 'Image-api JS', url: 'https://image-appi.netlify.com/' }} />
+        <Task task={{ name: 'Repair Design Project - SCSS', url: 'https://repair-project.netlify.com/' }} />
+        <Task task={{ name: 'The Yalow - CSS', url: 'https://the-yalow.netlify.com//' }} />
+        <Task task={{ name: 'Hexal - CSS', url: 'https://hexalogon.netlify.com' }} />
+        <Task task={{ name: 'NeutronMail- CSS', url: 'https://neutron-email.netlify.com/' }} />
       </div>
     </div>
   );
