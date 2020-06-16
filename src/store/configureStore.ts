ab99a0c2dc60dfaee144ed9f652cea38ef36a565
ramp4 <rampatv13@gmail.com>
2020-06-16 20:19:32 +0300

    delete: saga middleware

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "../reducers/index";
 // import * as mySaga from "../sagas/index";
 import thunk from "redux-thunk";
-const sagaMiddleware = createSagaMiddleware();
 // export const store = createStore(
 //   rootReducer,
 //   applyMiddleware(thunk, sagaMiddleware)
