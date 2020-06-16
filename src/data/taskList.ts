e7e17de60d07cacc7b6433b880768927cd972b06
ramp4 <47815558+ramp4@users.noreply.github.com>
2020-06-16 20:02:58 +0300

    Update taskList.ts

--- src/data/taskList.ts
+++ src/data/taskList.ts
@@ -1,47 +1,52 @@
 export default [
   {
+    name: "CEO Group test",
+    url: "https://ggroup-test.netlify.app/",
+    description: "Html, Css, Js",
+  },
+  {
     name: "Aviasales Tickets",
     url: "https://aviascales.netlify.com/",
-    description: "React, Redux Saga, TypeScript, SCSS, AviaSales API"
+    description: "React, Redux Saga, TypeScript, SCSS, AviaSales API",
   },
   {
     name: "Fancy Weather",
     url: "https://vveather.netlify.com/",
-    description: "JavaScript,  several weather API"
+    description: "JavaScript,  several weather API",
   },
   {
     name: "VK Api",
     url: "https://vk-api.netlify.com/",
-    description: "React, Redux, JavaScript, VK API"
+    description: "React, Redux, JavaScript, VK API",
   },
   {
     name: "News Page",
     url: "https://news-page.netlify.com/",
-    description: "React, Redux, JavaScript"
+    description: "React, Redux, JavaScript",
   },
   {
     name: "Image-Api",
     url: "https://image-appi.netlify.com/",
-    description: "Simple app for draw image"
+    description: "Simple app for draw image",
   },
   {
     name: "Repair Design Project",
     url: "https://repair-project.netlify.com/",
-    description: "Responsive layout without logic"
+    description: "Html/Scss",
   },
   {
     name: "The Yalow",
     url: "https://the-yalow.netlify.com//",
-    description: "Adaptive layout without logic"
+    description: "Html/Css",
   },
   {
     name: "Hexal",
     url: "https://hexalogon.netlify.com",
-    description: "Static layout without logic"
+    description: "Html/Css",
   },
   {
     name: "NeutronMail",
     url: "https://neutron-email.netlify.com/",
-    description: "Static layout without logic"
-  }
+    description: "Html/Css",
+  },
 ];
