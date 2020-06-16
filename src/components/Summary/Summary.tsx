1d09e75db0015c39768b33860acf4d65cf5c6289
ramp4 <47815558+ramp4@users.noreply.github.com>
2020-06-16 20:02:22 +0300

    Update Summary.tsx

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -5,27 +5,39 @@ class Summary extends React.PureComponent {
   render() {
     return (
       <div className="Summary">
-        <p className="Summary__title">Junior Front-End Developer Summary</p>
+        <p className="Summary__title">Developer Summary</p>
         <p className="Summary__description">
-          Hello! My name is Artem Balunda, and I'm a Minsk-based Front-End
-          Developer. I am currently studying at Belarusian State University of
-          Informatics and Radioelectronics in Faculty of Computer Design. I
-          recently finished online course "JavaScript/Front-end" of The Rolling
-          Scopes School.
+          I'm a Minsk-based <b>final</b> year student. I am studying at
+          Belarusian State University of Informatics and Radioelectronics in
+          Faculty of Computer Design. I recently finished "JavaScript/Front-end"
+          Course The Rolling Scopes School(
+          <a href="https://app.rs.school/certificate/3w8u2ak5">
+            https://app.rs.school/certificate/3w8u2ak5
+          </a>
+          ). And now i am looking for <b>internship / practice / job</b>. I want
+          to start a career as a Junior Developer and grow up to the next step.
+          I have a diverse set of skills, ranging from design, <b>HTML</b>{" "}
+          markup,
+          <b> CSS/SASS</b> styling, all the way to <b> JavaScript</b>, some
+          knowledge in
+          <b> TypeScript, React, Redux development</b>. I know what a <b>Git</b>{" "}
+          I know a little about
+          <b> Sql, Soql, Apex, SalesForce</b>. Of course, I have experience of
+          working in graphic editors like <b>Photoshop</b> and <b>Figma</b>. I
+          have good analytical skills. I'm a motivated fast learner. I want to
+          get practical development experience.
         </p>
         <p className="Summary__description">
-          I want to start a career as a Junior Front-End Developer and grow up
-          to the next step(Middle Frontend Developer).
+          <b>Education:</b> University: Belarusian State University of
+          Informatics and Radio electronics Faculty: Computer Design Specialty:
+          Electronic Security Systems
         </p>
         <p className="Summary__description">
-          I have a diverse set of skills, ranging from design, HTML markup,
-          CSS/SASS styling, all the way to JavaScript, TypeScript, React+Redux
-          development.
+          <b>English Skills:</b> Upper-Intermediate B1
         </p>
         <p className="Summary__description">
-          Of course, I have experience of working in graphic editors like
-          Photoshop and Figma. I have good analytical skills. I'm a fast
-          learner, good team worker.
+          <b>Contact e-mai</b>l:{" "}
+          <a href="mailto:rampatv13@gmail.com">rampatv13@gmail.com</a>
         </p>
       </div>
     );
